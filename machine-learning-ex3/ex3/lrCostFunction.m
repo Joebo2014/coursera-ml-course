3ad382ae3d7c2d5275fc4f4c719ccd00fc242e4f
Iman Tabrizian <tabrizian@outlook.com>
2018-08-31 00:32:02 +0430

    Add solution to week 4 questions

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,7 +36,10 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
+J = 1 / m .* (- y' * log(sigmoid(X * theta)) - (1 - y') * log(1 - sigmoid(X * theta)));
+J += lambda / (2 * m) * sum(theta(2:end) .^ 2);
+grad = (1 / m) * X' * (sigmoid(X * theta) - y);
+grad(2:end) += lambda / m * theta(2:end);
 
 
 
